2d3a9c2bf112dd6d2e225a7f940cd340c5c53aec
Amey Kulkarni <ameyk1510@gmail.com>
2023-05-14 20:08:14 +0530

    Adding random choice for delay interval - fixed mistake

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ led = machine.Pin(2, machine.Pin.OUT)
 
 valid_times = [0.25, 0.5, 0.75, 1, 1.5, 2]
 
-on_time = random.choice(valid_times)
-off_time = random.choice(valid_times)
-
 while True:
+	on_time = random.choice(valid_times)
+	off_time = random.choice(valid_times)
+
     led.value(1)
     print("LED ON for : ", str(on_time))
     time.sleep(on_time)
