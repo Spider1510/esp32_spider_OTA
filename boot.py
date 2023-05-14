c71cc9245b3c595d32c8dd293b18e73f84406aa3
Amey Kulkarni <ameyk1510@gmail.com>
2023-05-14 19:27:05 +0530

    commit from CLI v2

--- boot.py
+++ boot.py
@@ -21,10 +21,13 @@ with open("ota_status.txt", "w") as f:
 
 led = machine.Pin(2, machine.Pin.OUT)
 
+on_time = 0.5
+off_time = 0.5
 while True:
     led.value(1)
-    print("LED ON!")
-    time.sleep(2)
+    print("LED ON for : ", str(on_time))
+    time.sleep(on_time)
     led.value(0)
-    print("LED OFF!")
-    time.sleep(1)
+    print("LED OFF for : ", str(off_time))
+    time.sleep(off_time)
+	
