c237973aae1bddd8ed41ba2613366f2790e3f1e7
Amey Kulkarni <ameyk1510@gmail.com>
2023-05-14 15:51:55 +0530

    Update boot.py

--- boot.py
+++ boot.py
@@ -4,19 +4,19 @@ import os
 import ugit
 
 do_ota = "0"
-with open("ota_status", "r") as f:
+with open("ota_status.txt", "r") as f:
     do_ota = f.read()
     print("OTA staus : ", do_ota)
 
 if(do_ota == "1"):
-    with open("ota_status", "w") as f:
+    with open("ota_status.txt", "w") as f:
         f.write("0")
     ugit.wificonnect()
     # ugit.backup()
     ugit.pull_all()
 
 
-with open("ota_status", "w") as f:
+with open("ota_status.txt", "w") as f:
     f.write("1")
 
 led = machine.Pin(2, machine.Pin.OUT)
