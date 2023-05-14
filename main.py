322eb694959a80e6687d648128d1c839cd2fbc2f
Amey Kulkarni <ameyk1510@gmail.com>
2023-05-14 20:12:36 +0530

    Adding random choice for delay interval - fixed indentation errors v2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ valid_times = [0.25, 0.5, 0.75, 1, 1.5, 2]
 while True:
 	on_time = random.choice(valid_times)
 	off_time = random.choice(valid_times)
-	
+
 	led.value(1)
 	print("LED ON for : ", str(on_time))
 	time.sleep(on_time)
