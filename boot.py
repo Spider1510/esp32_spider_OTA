bc0d67a0c3b57d8275172ba127e4cb4586284598
Amey Kulkarni <ameyk1510@gmail.com>
2023-05-14 19:58:29 +0530

    seperating main and boot files

--- boot.py
+++ boot.py
@@ -19,15 +19,6 @@ if(do_ota == "1"):
 with open("ota_status.txt", "w") as f:
     f.write("1")
 
-led = machine.Pin(2, machine.Pin.OUT)
+print("OTA stuff complete!")
+print("Exiting boot.py")
 
-on_time = 0.5
-off_time = 0.5
-while True:
-    led.value(1)
-    print("LED ON for : ", str(on_time))
-    time.sleep(on_time)
-    led.value(0)
-    print("LED OFF for : ", str(off_time))
-    time.sleep(off_time)
-	
